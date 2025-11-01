e551d92ec1af61863d8ff166a7d9ebfbe9ed5ae0
Satria Sapta <satriasapta48@gmail.com>
2025-11-02 00:03:43 +0700

    creating authentication APIs

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
-require("dotenxv").config();
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -15,6 +17,13 @@ app.use(
 );
 
 app.use(express.json());
+
+connectDB();
+
+app.use("/api/v1/auth", authRoutes);
+
+//Serve uploads folder
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
