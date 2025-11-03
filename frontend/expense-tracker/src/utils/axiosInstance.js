561f26b4c86e5197e9662da1e995329b3e77ff9f
Satria Sapta <satriasapta48@gmail.com>
2025-11-03 22:19:19 +0700

    Creating an Axios Instance

---
+++ frontend/expense-tracker/src/utils/axiosInstance.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+import { BASE_URL } from "./apiPath";
+
+const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    timeout: 10000,
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    }
+});
+
+// Request Interceptopr
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const accessToken = localStorage.getItem("token");
+        if(accessToken) {
+            config.headers.Authorization = `Bearer ${accessToken}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+//Response Interceptor
+axiosInstance.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        // Handle common errors globally
+        if(error.response) {
+            if(error.response.status === 401) {
+                window.location.href = "/login";
+            } else if (error.response.status === 500) {
+                console.error("Seerver error. Please try again later");
+            }
+        } else if (error.code ==="ECONNABORTED") {
+            console.error("Request timed out. Please try again later");
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default axiosInstance;
