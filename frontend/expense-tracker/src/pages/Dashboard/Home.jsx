7004160a66c582dc01d90ba41179fa56bf47e885
Satria Sapta <satriasapta48@gmail.com>
2025-11-09 23:41:13 +0700

    Recent Transactions Section

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -9,6 +9,7 @@ import InfoCard from '../../components/Cards/InfoCard';
 import { LuHandCoins, LuWalletMinimal } from 'react-icons/lu';
 import {IoMdCard} from 'react-icons/io';
 import { addThousandsSeparator } from '../../utils/helper';
+import RecentTransactions from '../../components/Dashboard/RecentTransactions';
 
 const Home = () => {
   useUserAuth();
@@ -61,6 +62,13 @@ const Home = () => {
             label="Total Expense"
             value={addThousandsSeparator(dashboardData?.totalExpenses || 0)}
             color="bg-red-500"
+          />
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+          <RecentTransactions
+          transactions={dashboardData?.recentTransactions}
+          onSeeMore={() => navigate("/expense")}
           />
         </div>
       </div>
