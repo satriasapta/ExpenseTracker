932152ecbba039fe118d18904582b9e278f98a75
Satria Sapta <satriasapta48@gmail.com>
2025-11-11 22:46:12 +0700

    Financial Overview Section

--- frontend/expense-tracker/src/components/layouts/DashboardLayout.jsx
+++ frontend/expense-tracker/src/components/layouts/DashboardLayout.jsx
@@ -10,7 +10,7 @@ const DashboardLayout = ({children, activeMenu}) => {
         <Navbar activeMenu={activeMenu} />
         {user && (
             <div className="flex">
-                <div className="max[1080px]:hidden">
+                <div className="max-[1080px]:hidden">
                     <SideMenu activeMenu={activeMenu} />
                 </div>
                 
--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -10,6 +10,7 @@ import { LuHandCoins, LuWalletMinimal } from 'react-icons/lu';
 import {IoMdCard} from 'react-icons/io';
 import { addThousandsSeparator } from '../../utils/helper';
 import RecentTransactions from '../../components/Dashboard/RecentTransactions';
+import FinanceOverview from '../../components/Dashboard/FinanceOverview';
 
 const Home = () => {
   useUserAuth();
@@ -69,6 +70,12 @@ const Home = () => {
           <RecentTransactions
           transactions={dashboardData?.recentTransactions}
           onSeeMore={() => navigate("/expense")}
+          />
+
+          <FinanceOverview
+          totalBalance={dashboardData?.totalBalance || 0}
+          totalIncome={dashboardData?.totalIncome || 0}
+          totalExpense={dashboardData?.totalExpenses || 0}
           />
         </div>
       </div>
